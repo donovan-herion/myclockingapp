1a80a2e0b6c113e3e1a6521ffbe0d57ca253ad1a
donovanherion <donovanherion@gmail.com>
2020-09-05 15:36:10 +0200

    import post.php

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 
-
-
-
-
 // ba4ff18d681fdd login
 
 // 2a09e13a mdp
@@ -18,38 +14,7 @@ $bdd = new PDO("mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_7a112bc76d6
 
 #condition d'envoi a la bdd
 
-if (isset($_POST['commencer'])) {
-    $start = $bdd->prepare("INSERT INTO `commencer` (`commencer`) VALUES (?);");
-    $start->execute(array(date("Y-m-d H:i:s", strtotime('+2 hours'))));
-    $_POST['commencer'] = NULL;
-}
-
-
-    
-    if (isset($_POST['finir'])) {
-        $finish = $bdd->prepare("INSERT INTO `finir` (`finir`) VALUES (?);");
-        $finish->execute(array(date("Y-m-d H:i:s", strtotime('+2 hours'))));
-        $_POST['finir'] = NULL;
-
-
-        $tableau = $bdd->query("SELECT * FROM commencer ORDER BY commencer DESC LIMIT 1");
-        while ($row = $tableau->fetch(PDO::FETCH_ASSOC)) {
-          $strtotimecommencer = strtotime($row['commencer']); 
-          $strtotimefinir = strtotime(date("Y-m-d H:i:s", strtotime('+2 hours')));
-         }
-
-        
-        $totalsoustrait = $bdd->prepare("INSERT INTO `total` (`total`) VALUES (?);");
-        $totalsoustrait->execute(array($strtotimefinir - $strtotimecommencer));
-}
-
-
-if (isset($_POST['reset'])) {
-    $reset = $bdd->query("TRUNCATE TABLE `commencer`;");
-    $reset = $bdd->query("TRUNCATE TABLE `finir`;");
-    $reset = $bdd->query("TRUNCATE TABLE `total`;");
-    $_POST['reset'] = NULL;
-}
+include 'post.php';
 
 ?>
 
